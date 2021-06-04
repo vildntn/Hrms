c35943b09cade02ea83485546645e5ecc44eb55a
Lenovo <Lenovo@DESKTOP-TCNT505>
2021-06-05 00:10:14 +0300

    05.06.2021

--- src/main/java/kodlamaio/hrms/dataAccess/abstracts/CurriculumVitaeDao.java
+++ src/main/java/kodlamaio/hrms/dataAccess/abstracts/CurriculumVitaeDao.java
@@ -15,18 +15,10 @@ import kodlamaio.hrms.entities.dtos.CurriculumVitaeDto;
 public interface CurriculumVitaeDao  extends JpaRepository<CurriculumVitae, Integer> {
 
 	
-//deneme dto silindi
-	
-//	 @Query("Select new kodlamaio.hrms.entities.dtos.CvDto"
-//		  		+ "(c.candidate.id, e.branchName, e.educationName, c.githubUrl, j.companyName) "
-//		  		+ "From CurriculumVitae c Inner Join c.educations e Inner Join c.jobExperiences j")
-//		  List<CvDto> getCv();
+
 	
 	
-//	 @Query("Select new kodlamaio.hrms.entities.dtos.CurriculumVitaeDto"
-//		  		+ "(c.educations, c.jobExperiences) "
-//		  		+ "From CurriculumVitae c Inner Join c.educations e Inner Join c.jobExperiences j  Where c.candidate.id=:id")
-//	List<CurriculumVitaeDto> getCurriculumVitaeDto(int id);
+
 	 
 	 
 }
